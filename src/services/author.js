deb43ac9e09cd96b7fb7ddba46931ba496adbe74
Leonardo <109601810+Lramosdlima@users.noreply.github.com>
2023-10-04 23:31:42 -0300

    refactor: :recycle: change response api default

--- src/services/author.js
+++ src/services/author.js
@@ -1,74 +1,98 @@
 const authorModel = require('../models/author');
+const ResponseOn = require('../config/utils/response');
+
+const response = new ResponseOn();
 
 const getAll = async () => {
     try {
         const authors = await authorModel.getAll();
 
         if (authors.length === 0) {
-            return 'Nenhum autor encontrado';
+            return response.error('Nenhum autor encontrado', 404);
         }
-        return authors;
+
+        return response.success(authors, 200);
+
     } catch (error) {
-        return error;
+        return response.error(error, 500);
     }
 };
 
 const getById = async (id) => {
     try {
+        if (!id) {
+            return response.error('O id é obrigatório', 400);
+        }
+
         const author = await authorModel.getById(id);
 
         if (!author) {
-            return `Autor de id ${id} não encontrado`;
+            return response.error(`Autor de id ${id} não encontrado`, 404);
         }
 
-        return author;
+        return response.success(author, 200);
     } catch (error) {
-        return error;
+        return response.error(error, 500);
     }
 };
 
 const create = async (name, description) => {
     try {
+        if (!name || !description) {
+            return response.error('O nome e a descrição são obrigatórios', 400);
+        }
+
         const checkAuthorExist = await authorModel.getByName(name);
 
         if (!checkAuthorExist) {
-            return 'O autor já existe';
+            return response.error('O autor já existe', 400);
         }
 
-        const createdAuthor = await authorModel.create(name, description);
-        return createdAuthor;
+        await authorModel.create(name, description);
+
+        return response.success('Autor foi criado com sucesso', 201);
     } catch (error) {
-        return error;
+        return response.error(error, 500);
     }
 };
 
 const update = async (id, name, description) => {
     try {
+        if (!id || !name || !description) {
+            return response.error('O id, o nome e a descrição são obrigatórios', 400);
+        }
+
         const checkAuthorExist = await authorModel.getById(id);
 
         if (!checkAuthorExist) {
-            return `Autor de id ${id} não encontrado`;
+            return response.error(`Autor de id ${id} não encontrado`, 404);
         }
         
-        const updatedAuthor = await authorModel.update(id, name, description);
-        return updatedAuthor;
+        await authorModel.update(id, name, description);
+
+        return response.success('Autor foi atualizado com sucesso', 200);
     } catch (error) {
-        return error;
+        return response.error(error, 500);
     }
 };
 
 const exclude = async (id) => {
     try {
+        if (!id) {
+            return response.error('O id é obrigatório', 400);
+        }
+
         const checkAuthorExist = await authorModel.getById(id);
 
         if (!checkAuthorExist) {
-            return `Autor de id ${id} não encontrado`;
+            response.error(`Autor de id ${id} não encontrado`, 404);
         }
 
         await authorModel.exclude(id);
-        return 'Autor excluído com sucesso';
+
+        return response.success('Autor foi excluído com sucesso', 200);
     } catch (error) {
-        return error;
+        return response.error(error, 500);
     }
 };
 
