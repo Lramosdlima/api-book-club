23a3bea620139ed188d248f588cf228f581b143e
Leonardo <109601810+Lramosdlima@users.noreply.github.com>
2023-10-04 22:33:04 -0300

    feat: :sparkles: create service book

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -1,33 +1,33 @@
-const bookModel = require('../models/book');
+const bookService = require('../services/book');
 
 class BookController {
     async getAll(req, res) {
-        const books = await bookModel.getAll();
+        const books = await bookService.getAll();
         return res.status(200).json(books);
     }
 
     async getById(req, res) {
         const { id } = req.params;
-        const book = await bookModel.getById(id);
+        const book = await bookService.getById(id);
         return res.status(200).json(book);
     }
 
     async create(req, res) {
         const { title, synopsis, genre_id } = req.body;
-        const book = await bookModel.create(title, synopsis, genre_id);
+        const book = await bookService.create(title, synopsis, genre_id);
         return res.status(201).json(book);
     }
 
     async update(req, res) {
         const { id } = req.params;
         const { title, synopsis, genre_id } = req.body;
-        const result = await bookModel.update(id, title, synopsis, genre_id);
+        const result = await bookService.update(id, title, synopsis, genre_id);
         return res.status(200).json(result);
     }
 
     async exclude(req, res) {
         const { id } = req.params;
-        const result = await bookModel.exclude(id);
+        const result = await bookService.exclude(id);
         return res.status(200).json(result);
     }
 }
