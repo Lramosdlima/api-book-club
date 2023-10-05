431635494238bb926f88d060c6f03024aef85f9e
Rafael <rafael.caneca2001@gmail.com>
2023-10-04 21:48:39 -0300

    feat: :sparkles: new table community

---
+++ src/models/community.js
@@ -0,0 +1,48 @@
+const connection = require('/connection');
+
+const getAll = async () => {
+    const communitys = await connection.execute(
+        'SELECT * FROM community',
+    );
+    return communitys;
+};
+
+const getById = async (id) => {
+    const community = await connection.execute(
+        'SELECT * FROM community WHERE id = ?',
+        [id],
+    );
+    return community;
+};
+
+const create = async (name, description) => {
+    const community = await connection.execute(
+        'INSERT INTO community (name,description) VALUES (?, ?)',
+        [name, description],
+    );
+    return community;
+};
+
+const update = async (id, name, description) => {
+    const community = await connection.execute(
+        'UPDATE community SET name = ?, description = ? WHERE id = ?',
+        [name, description, id],
+    );
+    return community;
+};
+
+const exclude = async (id) => {
+    const community = await connection.execute(
+        'DELETE FROM community WHERE id = ?',
+        [id],
+    );
+    return community;
+};
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    exclude,
+};
