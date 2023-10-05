973902defcee3bfbc15ee4363ff5d07611380d09
flprbr <feliperbr99@gmail.com>
2023-10-04 22:06:37 -0300

    feat: :sparkles: create new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 const { Router } = require('express');
 const BookRouter = require('./book');
+const AuthorRouter = require('./author');
 
 const router = Router();
 
 router.use('/book', BookRouter);
+router.use('/author', AuthorRouter);
+router.use('/community', CommunityRouter);
+
 
 module.exports = router;
