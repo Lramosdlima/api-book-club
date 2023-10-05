8e12004054266cf43a9c5f3dc2f5fc4434501c61
Leonardo <109601810+Lramosdlima@users.noreply.github.com>
2023-10-04 23:18:57 -0300

    refactor: :recycle: add response api default

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -2,33 +2,33 @@ const bookService = require('../services/book');
 
 class BookController {
     async getAll(req, res) {
-        const books = await bookService.getAll();
-        return res.status(200).json(books);
+        const { codehttp, ...rest } = await bookService.getAll();
+        return res.status(codehttp).json(rest);
     }
 
     async getById(req, res) {
         const { id } = req.params;
-        const book = await bookService.getById(id);
-        return res.status(200).json(book);
+        const { codehttp, ...rest } = await bookService.getById(id);
+        return res.status(codehttp).json(rest);
     }
 
     async create(req, res) {
         const { title, synopsis, genre_id } = req.body;
-        const book = await bookService.create(title, synopsis, genre_id);
-        return res.status(201).json(book);
+        const { codehttp, ...rest } = await bookService.create(title, synopsis, genre_id);
+        return res.status(codehttp).json(rest);
     }
 
     async update(req, res) {
         const { id } = req.params;
         const { title, synopsis, genre_id } = req.body;
-        const result = await bookService.update(id, title, synopsis, genre_id);
-        return res.status(200).json(result);
+        const { codehttp, ...rest } = await bookService.update(id, title, synopsis, genre_id);
+        return res.status(codehttp).json(rest);
     }
 
     async exclude(req, res) {
         const { id } = req.params;
-        const result = await bookService.exclude(id);
-        return res.status(200).json(result);
+        const { codehttp, ...rest } = await bookService.exclude(id);
+        return res.status(codehttp).json(rest);
     }
 }
 
--- src/services/book.js
+++ src/services/book.js
@@ -1,56 +1,98 @@
 const bookModel = require('../models/book');
+const ResponseOn = require('../config/utils/response');
+
+const response = new ResponseOn();
 
 const getAll = async () => {
     try {
         const books = await bookModel.getAll();
 
         if (books.length === 0) {
-            return 'Nenhum livro encontrado';
+            return response.error('Nenhum livro encontrado', 404);
         }
-        return books;
+
+        return response.success(books, 200);
+
     } catch (error) {
-        return error;
+        return response.error(error, 500);
     }
 };
 
 const getById = async (id) => {
     try {
+        if (!id) {
+            return response.error('O id é obrigatório', 400);
+        }
+
         const book = await bookModel.getById(id);
 
         if (!book) {
-            return `Livro de id ${id} não encontrado`;
+            return response.error(`Livro de id ${id} não encontrado`, 404);
         }
 
-        return book;
+        return response.success(book, 200);
     } catch (error) {
-        return error;
+        return response.error(error, 500);
     }
 };
 
-const create = async (title, synopsis, genre_id) => {
+const create = async (name, description) => {
     try {
-        const book = await bookModel.create(title, synopsis, genre_id);
-        return book;
+        if (!name || !description) {
+            return response.error('O nome e a descrição são obrigatórios', 400);
+        }
+
+        const checkBookExist = await bookModel.getByName(name);
+
+        if (!checkBookExist) {
+            return response.error('O livro já existe', 400);
+        }
+
+        await bookModel.create(name, description);
+
+        return response.success('Livro foi criado com sucesso', 201);
     } catch (error) {
-        return error;
+        return response.error(error, 500);
     }
 };
 
-const update = async (id, title, synopsis, genre_id) => {
+const update = async (id, name, description) => {
     try {
-        const result = await bookModel.update(id, title, synopsis, genre_id);
-        return result;
+        if (!id || !name || !description) {
+            return response.error('O id, o nome e a descrição são obrigatórios', 400);
+        }
+
+        const checkBookExist = await bookModel.getById(id);
+
+        if (!checkBookExist) {
+            return response.error(`Livro de id ${id} não encontrado`, 404);
+        }
+        
+        await bookModel.update(id, name, description);
+
+        return response.success('Livro foi atualizado com sucesso', 200);
     } catch (error) {
-        return error;
+        return response.error(error, 500);
     }
 };
 
 const exclude = async (id) => {
     try {
+        if (!id) {
+            return response.error('O id é obrigatório', 400);
+        }
+
+        const checkBookExist = await bookModel.getById(id);
+
+        if (!checkBookExist) {
+            response.error(`Livro de id ${id} não encontrado`, 404);
+        }
+
         await bookModel.exclude(id);
-        return 'Livro excluído com sucesso';
+
+        return response.success('Livro foi excluído com sucesso', 200);
     } catch (error) {
-        return error;
+        return response.error(error, 500);
     }
 };
 
