6cdaecc9358bf666c0110021fe5314bc467a36b7
Leonardo <lramoslima97@gmail.com>
2023-11-30 19:05:10 -0300

    chore: :wrench: response when get base url

--- src/server.js
+++ src/server.js
@@ -3,9 +3,17 @@ const router = require('./routes');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use(express.json());
+
 app.use(router);
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'API Book Club',
+        time: new Date().toString(),
+    });
+});
