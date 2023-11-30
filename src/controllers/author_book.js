02b36299d6a00245da5dee89ce1b9ccd00125cec
Leonardo <lramoslima97@gmail.com>
2023-11-30 13:54:34 -0300

    style: :art: format code

--- src/controllers/author_book.js
+++ src/controllers/author_book.js
@@ -1,37 +1,37 @@
-const author_bookService = require('../services/author_book');
+const authorBookService = require('../services/author_book');
 
-class author_bookController {
+class AuthorBookController {
     async getAll(req, res) {
-        const { codehttp, ...rest } = await author_bookService.getAll();
+        const { codehttp, ...rest } = await authorBookService.getAll();
         return res.status(codehttp).json(rest);
     }
 
     async getById(req, res) {
         const { id } = req.params;
-        const { codehttp, ...rest } = await author_bookService.getById(id);
+        const { codehttp, ...rest } = await authorBookService.getById(id);
         return res.status(codehttp).json(rest);
     }
 
     async create(req, res) {
         const { book_id, author_id } = req.body;
-        const { codehttp, ...rest } = await author_bookService.create(book_id, author_id );
+        const { codehttp, ...rest } = await authorBookService.create(book_id, author_id );
         return res.status(codehttp).json(rest);
     }
 
     async update(req, res) {
         const { id } = req.params;
         const { book_id, author_id } = req.body;
-        const { codehttp, ...rest } = await author_bookService.update(id, book_id, author_id);
+        const { codehttp, ...rest } = await authorBookService.update(id, book_id, author_id);
         return res.status(codehttp).json(rest);
     }
 
     async exclude(req, res) {
         const { id } = req.params;
-        const { codehttp, ...rest } = await author_bookService.exclude(id);
+        const { codehttp, ...rest } = await authorBookService.exclude(id);
         return res.status(codehttp).json(rest);
     }
 }
 
 module.exports = {
-    author_bookController,
+    AuthorBookController,
 };
--- src/controllers/book_quiz.js
+++ src/controllers/book_quiz.js
@@ -1,37 +1,37 @@
-const book_quizService = require('../services/book_quiz');
+const bookQuizService = require('../services/book_quiz');
 
-class book_quizController {
+class BookQuizController {
     async getAll(req, res) {
-        const { codehttp, ...rest } = await book_quizService.getAll();
+        const { codehttp, ...rest } = await bookQuizService.getAll();
         return res.status(codehttp).json(rest);
     }
 
     async getById(req, res) {
         const { id } = req.params;
-        const { codehttp, ...rest } = await book_quizService.getById(id);
+        const { codehttp, ...rest } = await bookQuizService.getById(id);
         return res.status(codehttp).json(rest);
     }
 
     async create(req, res) {
-        const { book_id, quiz_id,title,description  } = req.body;
-        const { codehttp, ...rest } = await book_quizService.create(book_id, quiz_id,title,description );
+        const { book_id, quiz_id, title, description  } = req.body;
+        const { codehttp, ...rest } = await bookQuizService.create(book_id, quiz_id, title, description );
         return res.status(codehttp).json(rest);
     }
 
     async update(req, res) {
         const { id } = req.params;
-        const { book_id, quiz_id,title,description } = req.body;
-        const { codehttp, ...rest } = await book_quizService.update(id, book_id, quiz_id,title,description );
+        const { book_id, quiz_id, title, description } = req.body;
+        const { codehttp, ...rest } = await bookQuizService.update(id, book_id, quiz_id, title, description );
         return res.status(codehttp).json(rest);
     }
 
     async exclude(req, res) {
         const { id } = req.params;
-        const { codehttp, ...rest } = await book_quizService.exclude(id);
+        const { codehttp, ...rest } = await bookQuizService.exclude(id);
         return res.status(codehttp).json(rest);
     }
 }
 
 module.exports = {
-    book_quizController,
+    BookQuizController,
 };
