5cbcfe71a3273422169cb266b9cfc8fcd67e1e53
Lramosdlima <lramoslima97@gmail.com>
2024-04-04 16:45:21 -0300

    style: :art: order by name

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,9 @@ import { Router } from 'express';
 import AuthRouter from './auth';
 import AuthorRouter from './author';
 import BookRouter from './book';
-import InteractionRouter from './interaction';
 import CollectionRouter from './collection';
 import GenreRouter from './genre';
+import InteractionRouter from './interaction';
 import { Middleware } from './middleware';
 import TagRouter from './tag';
 import UserRouter from './user';
@@ -15,11 +15,11 @@ const router = Router();
 const middleware = new Middleware();
 
 router.use('/auth', AuthRouter);
-router.use('/book', middleware.auth, BookRouter);
-router.use('/interaction', middleware.auth, InteractionRouter);
 router.use('/author', middleware.auth, AuthorRouter);
-router.use('/genre', middleware.auth, GenreRouter);
+router.use('/book', middleware.auth, BookRouter);
 router.use('/collecion', middleware.auth, CollectionRouter);
+router.use('/genre', middleware.auth, GenreRouter);
+router.use('/interaction', middleware.auth, InteractionRouter);
 router.use('/tag', middleware.auth, TagRouter);
 router.use('/user', middleware.auth, UserRouter);
 
