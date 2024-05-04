ef563f1cb055ff4063939053b5a926d97c1491ef
Lramosdlima <lramoslima97@gmail.com>
2024-05-03 22:30:31 -0300

    refactor: :recycle: change to get collection public and others endpoints private

--- src/routes/collection.ts
+++ src/routes/collection.ts
@@ -1,22 +1,24 @@
 import { Router } from 'express';
 
 import { CollectionController } from '../controllers/collection';
+import { Middleware } from './middleware';
 
 const router = Router();
 
 const collectionController = new CollectionController();
+const middleware = new Middleware();
 
 router.get('/all', collectionController.getAll); // Todas coleções da comunidade
 router.get('/:id', collectionController.getById); // Uma coleção específica
-router.get('/user/:owner_id', collectionController.getAllByOwnerId); // Coleções que o usuário criou
+router.get('/user/:owner_id', middleware.auth, collectionController.getAllByOwnerId); // Coleções que o usuário criou
 
-router.post('/create', collectionController.create); // Cria uma nova coleção
-router.post('/user/add', collectionController.addCollectionToUser); // Adiciona uma coleção a um usuário
+router.post('/create', middleware.auth, collectionController.create); // Cria uma nova coleção
+router.post('/user/add', middleware.auth, collectionController.addCollectionToUser); // Adiciona uma coleção a um usuário
 
-router.patch('/update/:id', collectionController.update); // Atualiza nome ou descrição de uma coleção
-router.put('/add/book/:id', collectionController.addBookToCollection); // Adiciona um ou mais livros a uma coleção
+router.patch('/update/:id', middleware.auth, collectionController.update); // Atualiza nome ou descrição de uma coleção
+router.put('/add/book/:id', middleware.auth, collectionController.addBookToCollection); // Adiciona um ou mais livros a uma coleção
 
-router.delete('/delete/:id', collectionController.exclude); // Deleta uma coleção
-router.delete('/user/remove/:id', collectionController.removeCollectionFromUser); // Remove uma coleção de um usuário
+router.delete('/delete/:id', middleware.auth, collectionController.exclude); // Deleta uma coleção
+router.delete('/user/remove/:id', middleware.auth, collectionController.removeCollectionFromUser); // Remove uma coleção de um usuário
 
 export default router;
