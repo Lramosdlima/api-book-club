d4107b4933d33c34d38b6482ca3ba94644143029
Lramosdlima <lramoslima97@gmail.com>
2024-05-14 20:08:36 -0300

    chore: :wrench: add cors

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 import router from './routes';
 
@@ -15,6 +16,9 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use(morgan('tiny'));
 app.use(express.json());
+app.use(cors({
+    origin: process.env.FRONTEND_URL
+}));
 
 app.use(router);
 
